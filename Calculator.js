19408ce7fd36db6b7906d3feaa871a84bab39481
Replit user <>
2023-03-14 16:00:56 +0000

    adds more

--- Calculator.js
+++ Calculator.js
@@ -1,32 +1,31 @@
-class Calculator{
+class Calculator {
 
-  constructor()
-  {
-	console.log("Calculator loaded");
+  constructor() {
+    console.log("Calculator loaded");
   }
 
-add(num1=0,num2=0){
-   if(typeof(num1)!== 'number' ||
-   typeof(num2)!== 'number'){
-     return 0;
-   }
-   return num1 + num2;
- }
-
-
-  subtract(num1,num2=0){
-	return num1 - num2;
+  add(num1 = 0, num2 = 0) {
+    if (typeof (num1) !== 'number' ||
+      typeof (num2) !== 'number') {
+      return 0;
+    }
+    return num1 + num2;
   }
 
-  multiply(num1,num2){
-	return num1 * num2;
+
+  subtract(num1, num2 = 0) {
+    return num1 - num2;
+  } add .
+
+  multiply(num1, num2) {
+    return num1 * num2;
   }
 
-  divide(num1,num2){
-	return num1 / num2;
+  divide(num1, num2) {
+    return num1 / num2;
   }
 
 }
-
+//m
 //required so data is visible externally
 module.exports = Calculator;
